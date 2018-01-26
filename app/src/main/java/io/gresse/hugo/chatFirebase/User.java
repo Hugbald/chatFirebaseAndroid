85bb761045cefe14bba26f03a8a74f0ce59c9fe4
Hugbald <clement.monestier@hotmail.fr>
2018-01-26 11:03:19 +0100

    upload du travail local

---
+++ app/src/main/java/io/gresse/hugo/chatFirebase/User.java
@@ -0,0 +1,15 @@
+package io.gresse.hugo.chatFirebase;
+
+/**
+ * Created by Hugo Gresse on 04/12/2017.
+ */
+
+class User {
+    public String name;
+    public String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
